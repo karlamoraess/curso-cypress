a663fcff8bd9b1f672e79c002694aac2dd09a0fd
karlamoraess <karlamoraes279@gmail.com>
2024-03-14 16:37:46 -0300

    primeiro teste

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a', 'Register now').click();
+    cy.get('[formcontrolname="email"]').type('catarina@email.com.br');
+    cy.get('[formcontrolname="fullName"]').type('Catarina Pessoa');
+    cy.get('[formcontrolname="userName"]').type('catarinap');
+    cy.get('[formcontrolname="password"]').type('catarina123');
+    cy.contains('button', 'Register').click();
   })
 })
