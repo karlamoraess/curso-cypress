85958756a7e56d0ac950fde5227f0a4ea0ce3814
karlamoraess <karlamoraes279@gmail.com>
2024-03-14 20:06:34 -0300

    teste em massa

--- cypress/e2e/data-testes.cy.js
+++ cypress/e2e/data-testes.cy.js
@@ -1,11 +1,18 @@
 describe('Página de cadastro', () => {
-    it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-      cy.visit('http://localhost:4200/#home')
-      cy.contains('a', 'Register now').click();
-      cy.get('[data-test="email"]').type('catarina@email.com.br');
-      cy.get('[data-test="fullName"]').type('Catarina Pessoa');
-      cy.get('[data-test="registerUserName"]').type('catarinap');
-      cy.get('[data-test="registerPassword"]').type('catarina123');
-      cy.contains('button', 'Register').click();
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/')
     })
-  })
+
+    const usuarios = require('../fixtures/usuarios.json')
+
+    usuarios.forEach(usuario => {
+        it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+            cy.get('[data-test="register"]').click();
+            cy.get('input[data-test="email"]').type(usuario.email);
+            cy.get('input[data-test="fullName"]').type(usuario.fullName);
+            cy.get('input[data-test="registerUserName"]').type(usuario.userName);
+            cy.get('input[data-test="registerPassword"]').type(usuario.password);
+            cy.get('[data-test="btnRegister"]').click();
+        })
+    })
+})
